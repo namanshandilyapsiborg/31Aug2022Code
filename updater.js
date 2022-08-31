f976e4aeaeb3dfd46f8eb6a04a0c0b45545f2bd1
namanshandilyapsiborg <naman.shandilya@psiborg.in>
2022-08-31 15:16:21 +0530

    5th

---
+++ updater.js
@@ -0,0 +1,14 @@
+const AutoGitUpdate = require("auto-git-update");
+const config = {
+  repository: "https://github.com/namanshandilyapsiborg/31Aug2022Code.git",
+  fromReleases: false,
+  tempLocation: "D:/PsiBorg",
+  token: "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",
+  //ignoreFiles: ['util/config.js'],
+  //executeOnComplete: 'C:/Users/scheg/Desktop/worksapce/AutoGitUpdate/startTest.bat',
+  exitOnComplete: true,
+};
+const updater = new AutoGitUpdate(config);
+//updater.autoUpdate();
+updater.forceUpdate();
+//updater.compareVersions();
