07be4725abcdba54e25c3202c5452cac099c0628
namanshandilyapsiborg <naman.shandilya@psiborg.in>
2022-09-02 12:06:51 +0530

    saps_frontend

--- app.js
+++ app.js
@@ -1,11 +1,393 @@
-const express = require("express")
-const app = express();
+const { pubnub } = require("./pubnub-module");
+const download = require("download");
+const unzipper = require("unzipper");
+// const path = require("path");
+const { exec } = require("child_process");
+const path = require("path");
+const fs = require("fs");
+const { SerialPort } = require("serialport");
+var Wifi = require("rpi-wifi-connection");
+var wifi = new Wifi();
+const port = new SerialPort({ path: "/dev/ttyS0", baudRate: 115200 });
+var qr = require("qr-image");
 
+var { Base64 } = require("./Base64");
 
-console.log("naman sir and aman sir and vishad sir and vidushi mam")
+//====================== For Led =========================//
+var Gpio = require("onoff").Gpio; //include onoff to interact with the GPIO
+var LED = new Gpio(26, "out"); //use GPIO pin 4, and specify that it is output
+var LED2 = new Gpio(6, "out"); //===> For QR Code LED GLOWING
 
-console.log("naman sir and aman sir and vishad sir and vidushi mam")
+//====================== For Qr COde ===========================//
+const button = new Gpio(5, "in", "both");
 
-app.listen(4000, ()=>{
-    console.log('http://localhost:4000')
-})
+const { ReadlineParser } = require("@serialport/parser-readline");
+
+// let a = ["jNWN2kTOwITNmBDN"];
+// pubnub.subscribe({
+//   channels: a,
+// });
+let publishChannel ;
+let a=[];
+
+//========================= Getting MAcID ======================//
+function getChannel()
+{
+if (fs.existsSync("./realmacadd.json")) {
+  console.log("//=== macadress Channel exist ==//");
+  let data = fs.readFileSync("./realmacadd.json", "utf-8");
+  console.log("MAC id inside mac address json file ==> ", JSON.parse(data));
+  let mcadd = JSON.parse(data);
+  console.log("mcadd inside get_mac ===> ", mcadd[0].macaddress);
+  //==================== Mac address to write to the device ======================//
+  publishChannel = mcadd[0].macaddress
+  a.push(mcadd[0].macaddress);
+  pubnub.subscribe({
+    channels: a,
+  });
+}
+}
+
+getChannel()
+
+//========================= PUBNUB LISTENER ====================//
+pubnub.addListener({
+  status: function (statusEvent) {
+    if (statusEvent.category === "PNNetworkDownCategory") {
+      console.log("PNNetworkDownCategory ===> ", statusEvent.category);
+      pubnub.reconnect()
+    }
+    if (statusEvent.category === "PNConnectedCategory") {
+      console.log("statusEvent ===> ", statusEvent.category);
+    } else {
+      console.log("//== Connection failed ===//");
+    }
+  },
+  category: function (e) {
+    console.log(e.category === "PNNetworkDownCategory");
+  },
+  message: function (messageEvent) {
+    console.log("Message From Pubnub ===> ", messageEvent.message);
+    if (messageEvent.message.eventname === "download_video") {
+      DownloadVideoZip(
+        // ==> Download Function
+        messageEvent.message.fileurl,
+        messageEvent.message.uniquefilename,
+        messageEvent.message.filetype
+      );
+    }
+    if (messageEvent.message.eventname == "delete_user_file") {
+      //console.log("Eventname => ", messageEvent.message.eventname);
+      DeleteUserFiles(
+        messageEvent.message.uniquename,
+        messageEvent.message.filetype
+      );
+    }
+  },
+  presence: function (presenceEvent) {
+    console.log("Handle Presence ===> ", presenceEvent);
+  },
+});
+
+//==================== To Download Video ====================//
+function DownloadVideoZip(fileurl, zipname, filetype) {
+  console.log("Inside DownloadVideoZip ==> ", fileurl);
+  if (fileurl && zipname && filetype) {
+    const file = fileurl;
+
+    if (filetype == "video/mp4") {
+      console.log(" //=== Video/mp4 ======//");
+      const filePath = `${__dirname}/zippedfiles`;
+
+      download(file, filePath).then(() => {
+        console.log("//==   Video Download Completed   ==//");
+
+        //============ Now unzip the file ==================//
+        console.log("Inside Zip file name ==>", zipname);
+        const path = `./zippedfiles/${zipname}.zip`;
+        console.log("path ==>", path);
+
+        fs.createReadStream(path).pipe(
+          unzipper.Extract({ path: "./Saps_Rasp_Pubnub/src/Videos" })
+        );
+        //  else if (filetype == "image/jpeg") {
+        //   console.log(" //=== image/jpeg ======//");
+        //   fs.createReadStream(path).pipe(
+        //     unzipper.Extract({ path: "./Saps_Rasp_Pubnub/src/Images" })
+        //   );
+        // } else if (filetype == "image/png") {
+        //   console.log(" //=== image/png ======//");
+        //   fs.createReadStream(path).pipe(
+        //     unzipper.Extract({ path: "./Saps_Rasp_Pubnub/src/Pngimages" })
+        //   );
+        // }
+        setTimeout(() => {
+          fs.unlinkSync(`./zippedfiles/${zipname}.zip`, () => {
+            console.log("deleted");
+          });
+        }, 1000);
+      });
+    } else if (filetype == "image/jpeg") {
+      console.log("//========== Image type url ==============//");
+      const file = fileurl;
+      console.log("Image file url ==> ", fileurl);
+      //const filePath = `${__dirname}/zippedfiles`;
+      const filePath = `${__dirname}/Saps_Rasp_Pubnub/src/Images`;
+      download(file, filePath).then(() => {
+        console.log("//==  Image Download Completed   ==//");
+      });
+    }
+  }
+}
+
+//=================== Delet4e files ==========================//
+// Saps_Rasp_Pubnub/src/Videos
+function DeleteUserFiles(uniquefilename, filetype) {
+  console.log("//====== Delete user files ===== //");
+  if (filetype && uniquefilename) {
+    console.log("Filetype => ", filetype, "uniquefilename => ", uniquefilename);
+    let path1 =
+      filetype && filetype == "video/mp4"
+        ? path.join(
+            __dirname,
+            `/Saps_Rasp_Pubnub/src/Videos/${uniquefilename}.mp4`
+          )
+        : filetype && filetype == "image/jpeg"
+        ? path.join(
+            __dirname,
+            `/Saps_Rasp_Pubnub/src/Images/${uniquefilename}.jpg`
+          )
+        : null;
+    console.log("path == >", path1);
+    if (fs.existsSync(path1)) {
+      console.log("yes path exist");
+      fs.unlinkSync(path1, () => {
+        console.log("User File Has Been Deleted Successfully");
+      });
+    } else {
+      console.log("path doesn't exist");
+    }
+  }
+}
+
+//=============================== Auto Starting Backend =====================================//
+setTimeout(() => {
+  exec("npm start", { cwd: "./Saps_Rasp_Pubnub" }, (error, stdout, stderr) => {
+    if (error) console.log(error);
+    if (stdout) {
+      console.log("//====== stdout =========//");
+    } else {
+      console.log("else");
+    }
+    console.log("//======== inside exec =======//");
+  });
+}, 18000);
+
+setTimeout(() => {
+  exec("chromium-browser --app=http://www.localhost:3000/ --kiosk");
+}, 50000);
+
+//======================== UART MAC ID =======================================//
+
+function blinkLED() {
+  //function to start blinking
+  if (LED.readSync() === 0) {
+    //check the pin state, if the state is 0 (or off)
+    LED.writeSync(1); //set pin state to 1 (turn LED on)
+  } else {
+    LED.writeSync(0); //set pin state to 0 (turn LED off)
+  }
+}
+
+function endBlink() {
+  //function to stop blinking
+  //clearInterval(blinkInterval); // Stop blink intervals
+  LED.writeSync(0); // Turn LED off
+  //LED.unexport(); // Unexport GPIO to free resources
+}
+//============================== Button =================================//
+button.watch((err, value) => {
+  if (err) {
+    throw err;
+  }
+  console.log("value ===>", value);
+  if (value == 1) {
+    pubnub.publish(
+      {
+        channel: publishChannel,
+        message: {
+          eventname: "qrcode",
+          show: true,
+        },
+      },
+      (status, response) => {
+        console.log("Status Pubnub ===> ", status);
+        console.log("response Pubnub ====> ", response);
+      }
+    );
+  } else if (value == 0) {
+    pubnub.publish(
+      {
+        channel: publishChannel,
+        message: {
+          eventname: "qrcode",
+          show: false,
+        },
+      },
+      (status, response) => {
+        console.log("Status Pubnub ===> ", status);
+        console.log("response Pubnub ====> ", response);
+      }
+    );
+  }
+  LED2.writeSync(value);
+});
+
+// process.on("SIGINT", (_) => {
+//   LED2.unexport();
+//   button.unexport();
+// });
+
+//==============================================================================================//
+
+const parser = port.pipe(new ReadlineParser({ delimiter: "\r\n" }));
+
+parser.on("data", (data) => {
+  console.log("Data from port Device=====>", data);
+  if (data.includes("ssid")) {
+    let a = JSON.parse(data);
+    console.log("wifi credetainls ===> ", a);
+    console.log("ssid ==> ", a.ssid);
+    let wifiname = a.ssid;
+    let password = a.pass;
+    wifi
+      .connect({ ssid: wifiname, psk: password })
+      .then(() => {
+        console.log("Connected to network.");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  if (data.includes("Shutting Down")) {
+    console.log(" ----- shutting down getting hit ---- ");
+    //exec("sudo shutdown now")
+    exec("pkill -o chromium", (e, i) => {
+      if (e) {
+        console.log("error in Shutting down ===> ", e);
+      }
+      exec("sudo shutdown now");
+    });
+  }
+
+  if (data.includes("check_hang")) {
+    blinkLED();
+    let timer = setInterval(blinkLED, 250);
+    setTimeout(() => {
+      endBlink();
+      clearInterval(timer);
+    }, 5000);
+
+    const child = exec(
+      "ping -c 5 www.google.com",
+      function (error, stdout, stderr) {
+        if (error !== null) {
+          console.log("Not available");
+        } else {
+          console.log("Available");
+        }
+
+        port.write("pi_ok", function (err) {
+          if (err) {
+            return console.log("Error on Write: ", err.message);
+          }
+          console.log("message written");
+        });
+      }
+    );
+  }
+
+  if (data.includes("get_ip")) {
+    console.log(" ----- get_ip getting hit ---- ");
+    let IP = ip.address();
+    console.log("ip =====>", IP);
+    port.write(IP, function (err) {
+      if (err) {
+        return console.log("Error on Write: ", err.message);
+      }
+      console.log("message written");
+    });
+  }
+
+  //========================== For MacADD  =================================//
+  if (data.includes("get_mac")) {
+    let a = data;
+    console.log("wifi credetainls ===> ", a);
+    if (fs.existsSync("./realmacadd.json")) {
+      console.log("//=== macadress file exist ==//");
+      let data = fs.readFileSync("./realmacadd.json", "utf-8");
+      console.log("MAC id inside mac address json file ==> ", JSON.parse(data));
+      let mcadd = JSON.parse(data);
+      console.log("mcadd inside get_mac ===> ", mcadd);
+      //==================== Mac address to write to the device ======================//
+      port.write(`{\"pi_mac\":\"${mcadd[0]["macaddress"]}\"}`, function (err) {
+        if (err) {
+          return console.log("Error on Write: ", err.message);
+        }
+        console.log("//==== mac address has been send ===//");
+      });
+      getChannel();
+    } else {
+      console.log("//============ MAcadd file doesnot exist ============= //");
+      port.write('{"pi_mac":"NA"}', function (err) {
+        if (err) {
+          return console.log("Error on Write: ", err.message);
+        }
+        console.log("//==== mac address failed ===//");
+      });
+    }
+  }
+
+  if (data.includes("mac_add")) {
+    console.log("//============== device is asking for macadd ==========//");
+    console.log(
+      "mac address form the device ==> ",
+      JSON.parse(data)["mac_add"]
+    );
+    //==== Need to convert into Base64 encoded ==========//
+    console.log(
+      "Base 64 encoded ==> ",
+      Base64.encode(JSON.parse(data)["mac_add"])
+    );
+    let mcadd = Base64.encode(JSON.parse(data)["mac_add"]);
+
+    function reverseString(str) {
+      var splitString = str.split("");
+      var reverseArray = splitString.reverse();
+      var joinArray = reverseArray.join("");
+      return joinArray;
+    }
+    mcadd = reverseString(mcadd);
+    console.log("Reverse MAc id ==> ", mcadd);
+    let realmcadd = [];
+    jsonVariable = { macaddress: mcadd };
+    realmcadd.push(jsonVariable);
+    //========== Generating JSON file with Mac addr BAse 64 Encoded  ======//
+    fs.writeFileSync("./realmacadd.json", JSON.stringify(realmcadd), "utf-8");
+    fs.writeFileSync(
+      "./Saps_Rasp_Pubnub/src/macadd.json",
+      JSON.stringify(realmcadd),
+      "utf-8"
+    );
+
+    //============= Generating QRCode =================================//
+    var qr_code = qr.image(mcadd, { type: "png" });
+    qr_code.pipe(
+      require("fs").createWriteStream(
+        "./Saps_Rasp_Pubnub/src/Qrcode/qrcode.png"
+      )
+    );
+    getChannel();
+  }
+});
