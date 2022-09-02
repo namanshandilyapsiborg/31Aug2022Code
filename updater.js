4d9f27e132873bad10e881710fb16bcdbe0b7fe6
namanshandilyapsiborg <naman.shandilya@psiborg.in>
2022-09-02 10:48:36 +0530

    22th commit

--- updater.js
+++ updater.js
@@ -2,14 +2,14 @@ const AutoGitUpdate = require("auto-git-update");
 const { exec, spawn } = require("child_process");
 const { stdout, mainModule } = require("process");
 const config = {
-    repository: "https://github.com/namanshandilyapsiborg/31Aug2022Code.git",
+    repository: "https://github.com/namanshandilyapsiborg/31Aug2022Code",
     //branch : 'main',
     fromReleases: false,
     tempLocation: "D:/PsiBorg",
     //token: "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",
     //ignoreFiles: ['util/config.js'],
     //executeOnComplete: 'C:/Users/scheg/Desktop/worksapce/AutoGitUpdate/startTest.bat',
-    //executeOnComplete: print(),
+    executeOnComplete: print(),
     exitOnComplete: false,
 };
 const updater = new AutoGitUpdate(config);
@@ -25,13 +25,18 @@ try {
         if(versionChecker.currentVersion != versionChecker.remoteVersion)
         {
             console.log("//=== Verisons are not same ===//")
+
+        }
+        else if(versionChecker.upToDate == true)
+        {
+            console.log("//==== Version is UpDated ===//")
         }
     },20000)
 } catch (e) { console.log("E : ", e) }
 
 
 function print() {
-    console.log("print function got hit")
+    console.log("//=== print function got hit ====//")
     // setTimeout(()=>{
     //     const child = spawn('npm i',{
     //         stdio : 'inherit',
