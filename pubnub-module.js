7fa07c99fc179a3e48f0c29e3adaa0345bbcba26
namanshandilyapsiborg <naman.shandilya@psiborg.in>
2022-09-07 17:02:02 +0530

    46 ver

--- pubnub-module.js
+++ pubnub-module.js
@@ -4,7 +4,7 @@ const uuid = PubNub.generateUUID();
 const pubnub = new PubNub({
   publishKey: "pub-c-ee8d5114-572e-40bd-bb92-6b00b09fc202",
   subscribeKey: "sub-c-6c7df15a-c787-11ec-8c08-82b465a2b170",
-  uuid: uuid,
+  //uuid: uuid,
   restore: true,
   presenceTimeout: 20,
   autoNetworkDetection : true,
