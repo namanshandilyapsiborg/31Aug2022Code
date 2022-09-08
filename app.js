f014183594f152fea0b0d1c6e5968784d020a3a3
namanshandilyapsiborg <naman.shandilya@psiborg.in>
2022-09-08 16:49:40 +0530

    version

--- app.js
+++ app.js
@@ -193,21 +193,6 @@ function PlayPauseVideo(data)
                 );
              }
         }
-        if (data && data.filetype == "updating") 
-        {
-             if(frontendChannel)
-             {
-                pubnub.publish(
-                    {
-                        channel: frontendChannel,
-                        message: data,
-                    },
-                    (status, response) => {
-                        console.log("Status Pubnub ===> ", status);
-                    }
-                );
-             }
-        }
     }
     else if(data.eventname == "stop")
     {
@@ -223,6 +208,21 @@ function PlayPauseVideo(data)
                }
            );
         }
+    }
+    else if (data && data.filetype == "updating") 
+    {
+         if(frontendChannel)
+         {
+            pubnub.publish(
+                {
+                    channel: frontendChannel,
+                    message: data,
+                },
+                (status, response) => {
+                    console.log("Status Pubnub ===> ", status);
+                }
+            );
+         }
     }
 }
 
