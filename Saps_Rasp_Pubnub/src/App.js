393af4751e43478968fcd28aedfebdfb9f942c4a
namanshandilyapsiborg <naman.shandilya@psiborg.in>
2022-12-20 09:38:23 +0530

    3

--- Saps_Rasp_Pubnub/src/App.js
+++ Saps_Rasp_Pubnub/src/App.js
@@ -306,7 +306,7 @@ function App() {
                                <ReactPlayer
                                playing
                                width={"100%"}
-                               height={"100%"}
+                               height={"100vh"}
                                //url="https://www.youtube.com/watch?v=668nUCeBHyY"
                                url={videoname && videoname}
                                muted={true}
